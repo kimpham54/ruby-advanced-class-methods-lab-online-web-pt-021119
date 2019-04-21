31c560137eb041fd650633033ae7cd45607e1545
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-21 05:19:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,11 @@
+
+
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+  def self.all()
     @@all
   end
 
@@ -10,4 +13,77 @@ class Song
     self.class.all << self
   end
 
+
+  def self.create()
+    song = self.new
+    song.save
+    song
+    # @@all << song
+    # return song
 end
+
+def self.new_by_name(title)
+  song = self.new
+  song.name = title
+  song
+end
+
+def self.create_by_name(title)
+  song = self.create
+  song.name = title
+  song
+  # @@all.push(song)  song
+end
+
+def self.find_by_name(title)
+  # self.all.detect{|s| s.name == title}
+found = nil
+@@all.each do |song|
+  if song.name == title
+    found = song
+  end
+end
+found
+end
+
+def self.find_or_create_by_name(title)
+# self.find_by_name(title)
+# found = nil
+# @@all.each do |song|
+#   if song.name == title
+#     found = song
+#   end
+# end
+# found
+songlookup = nil
+songlookup = self.find_by_name(title)
+  if songlookup == nil
+    # self.create_by_name(title)
+      # song = self.new
+      # song.name = title
+      # @@all.push(song)
+      # song
+      songlookup = self.create_by_name(title)
+    end
+    songlookup
+end
+
+def self.alphabetical
+end
+
+def self.new_from_filename
+end
+
+def self.create_from_filename
+end
+
+def self.destroy_all
+  self.all.clear
+end
+
+
+
+end
+
+# blank_space = Song.find_by_name("Blank Space")
+# binding.pry
